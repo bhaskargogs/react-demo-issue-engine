5f06a4f204d360d5d3bf47fa2236ec805b18939a
Bhaskar Gogoi <bhaskargogoi24@gmail.com>
2019-12-14 21:31:38 -0500

    Initial model

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Search } from './Search/Search';
 
 function App() {
+  const issues = [
+    'Issue#1: Database Connectivity Feature',
+    'Issue#2: RestAPI call made and could\'t fetch data',
+    'Issue#3: Database servers are down'
+  ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Search content={issues} />
     </div>
   );
 }
---
+++ src/Search/Search.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+export class Search extends React.Component {
+    state = {
+        initialIssues: [],
+        issues: []
+    };
+
+    filterList = (event) => {
+        let issues = this.state.initialIssues;
+        issues = issues.filter((issue) => {
+            return issue.toLowerCase().search(event.target.value.toLowerCase()) !== -1;
+        });
+        this.setState({ issues: issues });
+    }
+
+    componentWillMount = () => {
+        this.setState({
+            initialIssues: this.props.content,
+            issues: this.props.content   
+        })
+    }
+    
+
+    render() {
+        return (
+            <div>
+                <h1>React Demo</h1>
+                <h2>Start typing to search issues</h2>
+                <form>
+                    <input type="text" onChange={this.filterList} />
+                </form>
+                <div>
+                {
+                    this.state.issues.map((issue) => {
+                    return <div key={issue}> {issue}</div>
+                    })
+                }
+                </div>
+            </div>
+        )
+    }
+}
+
